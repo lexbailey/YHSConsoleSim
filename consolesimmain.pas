7e7358132cc9c9404e22b861d577fc8ad8795338
danieljabailey <danieljabailey@gmail.com>
2014-02-27 14:10:54 +0000

    mostly added comments to main program

--- consolesimmain.pas
+++ consolesimmain.pas
@@ -31,7 +31,6 @@ type
     btnReload: TButton;
     btnAutoSubscribe: TButton;
     btnClearSubs: TButton;
-    btnCreateControls: TButton;
     btnPublish: TButton;
     cbUIUpdate: TCheckBox;
     edtPublishTopic: TEdit;
@@ -85,6 +84,7 @@ type
     procedure btnReloadClick(Sender: TObject);
     procedure cbUIUpdateChange(Sender: TObject);
     procedure fneLoadConfigAcceptFileName(Sender: TObject; var Value: String);
+    procedure fneLoadConfigChange(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
@@ -99,6 +99,7 @@ type
     procedure MQTTClientPublish(Sender: TObject; topic, payload: ansistring);
     procedure MQTTClientSubAck(Sender: TObject; MessageID: integer;
       GrantedQoS: integer);
+    procedure pnlLoadConfigClick(Sender: TObject);
     procedure pnlLoadConfigResize(Sender: TObject);
     procedure sbDrawingAreaPaint(Sender: TObject);
     procedure tmrStatTimer(Sender: TObject);
@@ -114,6 +115,7 @@ type
     procedure tvControlsClick(Sender: TObject);
     procedure tvControlsMouseUp(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
+    procedure InitialiseControls;
   private
     { private declarations }
   public
@@ -142,6 +144,8 @@ implementation
 {$R *.lfm}
 { TfrmMain }
 
+
+//for debugging
 procedure TfrmMain.log(info: string);
 begin
 //  mOutputOld.Append(info);
@@ -150,8 +154,15 @@ begin
   writeln(info);
 end;
 
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  //Give the spacehack controls access to our instance of the MQTT client object
+  spacehackcontrols.MQTTClient := MQTTClient;
+end;
+
 procedure TfrmMain.FormShow(Sender: TObject);
 begin
+
 end;
 
 procedure TfrmMain.gbStdSubClick(Sender: TObject);
@@ -159,24 +170,21 @@ begin
 
 end;
 
-procedure TfrmMain.FormClose(Sender: TObject; var CloseAction: TCloseAction);
-begin
-end;
-
-procedure TfrmMain.FormCreate(Sender: TObject);
-begin
-  spacehackcontrols.MQTTClient := MQTTClient;
-end;
-
 procedure TfrmMain.btnDisconnectClick(Sender: TObject);
 begin
+  //To disconnect...
+  //Unsubscribe from everything
   btnClearSubs.Click;
+  //disable the buttons for interacting with the server
   gbComServer.Enabled:=false;
   log('Disconnecting...');
+  //if we are connected...
   if MQTTClient.isConnected then begin
     log('Graceful disconnect...');
+    //try to disconnect nicely
     if not MQTTClient.Disconnect then begin
       log('Forecful disconnect...');
+      //if that fails then just abandon ship
       MQTTClient.ForceDisconnect;
     end;
   end;
@@ -184,25 +192,30 @@ end;
 
 procedure TfrmMain.btnPinShowClick(Sender: TObject);
 begin
-  frmPinShow.show;
+
 end;
 
 procedure TfrmMain.btnPublishClick(Sender: TObject);
 begin
+  //Allow user to publish arbitrary payloads to arbitrary topics
   MQTTClient.Publish(edtPublishTopic.Text, memPublishPayload.Text);
 end;
 
 procedure TfrmMain.btnRegisterClick(Sender: TObject);
 begin
+  //register with the game server simply by sending the interface section of the config to the server
   MQTTClient.Publish('server/register', interfaceConfigJSON.FormatJSON());
 end;
 
 procedure TfrmMain.subscribeTo(topic: string; isAdditional: boolean = false);
 begin
+  //subscribe to the requested topic
   MQTTClient.Subscribe(topic);
   if isAdditional then begin
+    //If this is a user request for an arbitrary topic then add it to the additional subscription list
     memAddSub.Append(topic);
   end else
+    //otherwise it should go in the standard list
     memStdSub.Append(topic);
 end;
 
@@ -211,10 +224,12 @@ var
   prefix: string;
   i: integer;
 begin
+  //subscribe to default topics
   prefix := 'clients/' + myIP + '/';
   subscribeTo(prefix + 'configure');
   subscribeTo(prefix + 'instructions');
 
+  //subscribe to all of the topics that are required for each control in the config
   for i := 0 to numControls-1 do
   begin
     if myspacehackControls[i].hardware <> 'instructions' then begin
@@ -228,6 +243,7 @@ procedure TfrmMain.btnClearSubsClick(Sender: TObject);
 var
   i: integer;
 begin
+  //loop through all subscriptions of each type and unsubscribe from them
   for i := 0 to memStdSub.Lines.Count-1 do begin
     MQTTClient.Unsubscribe(memStdSub.Lines[i]);
   end;
@@ -238,23 +254,28 @@ begin
   memAddSub.Clear;
 end;
 
-procedure TfrmMain.btnCreateControlsClick(Sender: TObject);
+procedure TfrmMain.InitialiseControls;
 var
   i: integer;
   thisControl: TSpacehackControl;
   thisPanel: TPanel;
   thisLabel: TLabel;
 begin
+  //free all of the controls we currently have from the panel 'pnlControls'
   for i := pnlControls.ControlCount-1 downto 0 do begin
     pnlControls.Controls[i].Free;
   end;
+  //make the panel as wide as possible
   pnlControls.Width:=sbDrawingArea.Width - 20;
+  //make it high enough for one control
   pnlControls.Height:=220;
   nextTop := 10;
   nextLeft := 10;
+  //loop through all controls loaded from the config
   for i := 0 to numControls-1 do
   begin
     thisControl := myspacehackControls[i];
+    //create a panel for each one
     thisPanel := TPanel.Create(pnlControls);
     with thisPanel do begin
       Width:=200;
@@ -264,6 +285,7 @@ begin
       Parent:= pnlControls;
       Visible:=true;
     end;
+    //put a label on the panel
     thisLabel := TLabel.Create(thisPanel);
     with thisLabel do begin
       top := 0;
@@ -274,12 +296,16 @@ begin
       font.Color:=clGray;
     end;
     thisPanel.Tag:=i;
+    //tell the control to initialise the panel to its liking
     thisControl.initUI(thisPanel);
+    //move for next control
     nextLeft += 210;
+    //if this control goes over the edge then move down one
     if nextLeft+210 >= pnlControls.Width then
     begin
       nextLeft := 10;
       nextTop += 210;
+      //make panel bigger to fit
       pnlControls.Height := pnlControls.Height + 210;
     end;
   end;
@@ -300,12 +326,12 @@ procedure TfrmMain.fneLoadConfigAcceptFileName(Sender: TObject;
 
 begin
   loadConfiguration(Value);
-
 end;
 
 procedure TfrmMain.btnConnectClick(Sender: TObject);
 begin
   log('Starting connection...');
+  //if connected, disconnect
   if MQTTClient.isConnected then begin
     if not MQTTClient.Disconnect then begin
       MQTTClient.ForceDisconnect;
@@ -314,9 +340,12 @@ begin
   log('Host: ' + eServer.Text);
   log('Port: ' + trim(ePort.Text));
   log('CID: ' + MQTTClient.ClientID);
+  //set the host name
   MQTTClient.Hostname:=eServer.Text;
+  //set the port
   MQTTClient.Port:=strToInt(trim(ePort.Text));
   log('Waiting for connection...');
+  //connect
   MQTTClient.Connect;
 end;
 
@@ -331,16 +360,22 @@ procedure TfrmMain.miControlPinsClick(Sender: TObject);
 var index, numPins, i: integer;
 begin
   try
+    //get the index of the control that is selected
     index := tvControls.Selected.Index;
+    //prepare the pin diagram form for display
     frmPinShow.Caption:='Pin Finder - Control pins for '+myspacehackControls[index].hardware;
     numPins := myspacehackControls[index].pins.numPins;
+    //hide all of the pins on the pin diagram
     frmPinShow.clearPins;
     for i:= 0 to numPins-1 do begin
+      //show each pin found in the config for this control
       frmPinShow.activatePin(myspacehackControls[index].pins.pinIDs[i]);
     end;
+    //re-show the pin form
     frmPinShow.Hide;
     frmPinShow.Show;
   except
+    //if a pin has an invalid syntax (and is therefore undefined) aler the user
     ShowMessage('All pins must be defined');
   end;
 end;
@@ -349,6 +384,7 @@ procedure TfrmMain.miShowPinsClick(Sender: TObject);
 var index, numPins, i: integer;
 
 begin
+  //similar to above but for busses
   try
     index := tvControls.Selected.Index;
     frmPinShow.Caption:='Pin Finder - Bus pins for '+spacehackBuses[index].busName;
@@ -368,22 +404,20 @@ end;
 procedure TfrmMain.MQTTClientConnAck(Sender: TObject; ReturnCode: integer);
 begin
   log('Connection established, returned: '+ inttostr(ReturnCode));
+  //once connected, allow access to the server interaction buttons
   gbComServer.Enabled:=true;
-end;
-
-procedure TfrmMain.MQTTClientPingResp(Sender: TObject);
-begin
-  //log('Ping response');
 end;
 
 procedure TfrmMain.setInstruction(instruction: string);
 var
   i: integer;
 begin
+  //loop through all controls
   for i := 0 to numControls-1 do
   begin
     if myspacehackControls[i].hardware = 'instructions' then begin
       log('Instruction update');
+      //for every instrtuction display found, update the text
       TSPacehackInstructionDisplay(myspacehackControls[i]).instruction:=instruction;
     end;
   end;
@@ -393,10 +427,12 @@ procedure TfrmMain.setTimeout(timeout: extended);
 var
   i: integer;
 begin
+  //loop through all controls
   for i := 0 to numControls-1 do
   begin
     if myspacehackControls[i].hardware = 'instructions' then begin
       log('Instruction time update');
+      //for each instruction display found, update the time remaining
       TSPacehackInstructionDisplay(myspacehackControls[i]).timeAvailable:=round(timeout*1000);
       TSPacehackInstructionDisplay(myspacehackControls[i]).timeLeft:=round(timeout*1000);
     end;
@@ -406,14 +442,19 @@ end;
 procedure TfrmMain.tmrUIUpdateTimer(Sender: TObject);
 var
   i: integer;
+  instr: TSPacehackInstructionDisplay;
 begin
+  //timer to update the UI elements constantly
+  //loop through all controls
   for i := 0 to numControls-1 do
   begin
-    myspacehackControls[i].updateUI;
     if myspacehackControls[i].hardware = 'instructions' then begin
-       TSPacehackInstructionDisplay(myspacehackControls[i]).timeLeft:=
-       TSPacehackInstructionDisplay(myspacehackControls[i]).timeLeft - tmrUIUpdate.Interval;
+      //if an instruction display is found then decrement the time it is showing
+      instr := TSPacehackInstructionDisplay(myspacehackControls[i]);
+      instr.timeLeft := instr.timeLeft - tmrUIUpdate.Interval;
     end;
+    //update the UI for each control
+    myspacehackControls[i].updateUI;
   end;
 end;
 
@@ -421,10 +462,14 @@ procedure TfrmMain.handleControlUpdate(controlID: integer; topic, payload: strin
 var
   enable: boolean;
 begin
+  //this function called when 'name' or 'enabled' topic gets a payload for a given control 'controlID'
+
+  //update control names
   if topic = 'name' then begin
     log('Setting name');
     myspacehackControls[controlID].name:=payload;
   end;
+  //update enabled
   if topic = 'enabled' then begin
     enable := payload='1';
     TSpacehackGameControl(myspacehackControls[controlID]).Enabled:=enable;
@@ -443,40 +488,45 @@ var controlsJSON: TJSONObject;
   controlType: string;
   controlName: string;
 begin
-  controlsJSON := TJSONObject(roundConfig.Find('controls'));
-  numControls := controlsJSON.Count;
-  for i := 0 to numControls-1 do begin
-    thisID := i+1;
-    thisIDText := inttostr(thisID);
-    log ('testing ID: '+thisIDText);
-    thisControlJSON := controlsJSON.Objects[thisIDText];
-    if assigned(thisControlJSON) then begin
-      controlType := thisControlJSON.Strings['type'];
-      controlName := thisControlJSON.Strings['name'];
-      controlEnabled := thisControlJSON.Integers['enabled'];
+  //initiates a round based on the JSON config received from the 'config' topic
+  if (roundConfig <> nil) and (roundConfig.Find('controls') <> nil) then begin
+    controlsJSON := TJSONObject(roundConfig.Find('controls'));
+    numControls := controlsJSON.Count;
+    for i := 0 to numControls-1 do begin
+      //this mostly explains itself...
+      thisID := i+1;
+      thisIDText := inttostr(thisID);
+      log ('testing ID: '+thisIDText);
+      thisControlJSON := controlsJSON.Objects[thisIDText];
+      if assigned(thisControlJSON) then begin
+        controlType := thisControlJSON.Strings['type'];
+        controlName := thisControlJSON.Strings['name'];
+        controlEnabled := thisControlJSON.Integers['enabled'];
 
-      myspacehackControls[thisID].enabled:= controlEnabled = 1;
-      myspacehackControls[thisID].name := controlName;
-      TSpacehackGameControl(myspacehackControls[thisID]).controlType := controlType;
-      log('Control '+inttostr(thisID)+' is now ' + controlName + ', ' + controlType + ', Active: ' + inttostr(controlEnabled));
+        myspacehackControls[thisID].enabled:= controlEnabled = 1;
+        myspacehackControls[thisID].name := controlName;
+        TSpacehackGameControl(myspacehackControls[thisID]).controlType := controlType;
+        log('Control '+inttostr(thisID)+' is now ' + controlName + ', ' + controlType + ', Active: ' + inttostr(controlEnabled));
 
+      end;
     end;
+    setInstruction(roundConfig.Strings['instructions']);
+    //if a new timeout is defined then show it
+    if roundConfig.Find('timeout') <> nil then begin;
+      setTimeout(roundConfig.Floats['timeout']);
+    end else
+    begin
+      //if not then show no timeout
+      setTimeout(0);
+    end;
+
   end;
-  setInstruction(roundConfig.Strings['instructions']);
-
-  if roundConfig.Find('timeout') <> nil then begin;
-    setTimeout(roundConfig.Floats['timeout']);
-  end;
-end;
-
-procedure TfrmMain.tvControlsClick(Sender: TObject);
-begin
-
 end;
 
 procedure TfrmMain.tvControlsMouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
+  //check if a right click was on a bus or control that can be shown in the pin window
   if Button=mbRight then begin
     if assigned(tvControls.Selected) and assigned(tvControls.Selected.Parent) then begin
       if tvControls.Selected.Parent.Text = 'Buses' then begin
@@ -496,7 +546,13 @@ var
   controlID: integer;
   thisTopic: string;
 begin
+  //this event fires when a payload arrives at a topic to which this client is subscribed
   log(topic + ': ' + payload);
+  //topicParser splits the topic string into individual tokens e.g. client/ip/4/enabled will be split into:
+  // [0] = client
+  // [1] = ip
+  // [2] = 4
+  // [3] = enabled
   topicParser := TStringList.Create;
   topicParser.Delimiter:='/';
   topicParser.DelimitedText:=topic;
@@ -521,14 +577,19 @@ begin
               try
                 roundJSONParser := TJSONParser.Create(payload);
                 roundConfigJSON := TJSONObject(roundJSONParser.Parse);
-                initiateRound(roundConfigJSON);
+                if roundConfigJSON <> nil then begin
+                  initiateRound(roundConfigJSON);
+                end else
+                begin
+                  log('Invalid config received');
+                end;
               finally
                 roundConfigJSON.Free;
                 roundJSONParser.Free;
               end;
             end else
             begin
-              //lets assume that the topic is a control number
+              //lets assume that the next part of the topic is a control number
               try
                 controlID := strtoint(topicParser.Strings[2]);
                 if topicParser.Count>3 then begin
@@ -557,16 +618,13 @@ end;
 
 procedure TfrmMain.pnlLoadConfigResize(Sender: TObject);
 begin
+  //file name edit boxes are such a pain in the neck...
   fneLoadConfig.Width:=pnlLoadConfig.Width-26;
-end;
-
-procedure TfrmMain.sbDrawingAreaPaint(Sender: TObject);
-begin
-
 end;
 
 procedure TfrmMain.tmrStatTimer(Sender: TObject);
 begin
+  //this timer updates the status bar text
   if MQTTClient.isConnected then begin
     statusBar.SimpleText:='Connected';
     gbComServer.Enabled:=true;
@@ -598,6 +656,7 @@ begin
     controlID[i] := i;
     //store the hardware type
     hardwareType := controlJSON[i].Find('hardware').AsString;
+    //create the correct type of hardware
     if hardwareType = 'instructions' then myspacehackControls[i] := TSPacehackInstructionDisplay.create;
     if hardwareType = 'combo7SegColourRotary' then myspacehackControls[i] := TSpacehackGameControlCombo7SegColourRotary.create;
     if hardwareType = 'illuminatedtoggle' then myspacehackControls[i] := TSpacehackGameControlIlluminatedToggle.create;
@@ -605,12 +664,14 @@ begin
     if hardwareType = 'potentiometer' then myspacehackControls[i] := TSpacehackGameControlPotentiometer.create;
     if hardwareType = 'illuminatedbutton' then myspacehackControls[i] := TSpacehackGameControlIlluminatedButton.create;
     if hardwareType = 'keypad' then myspacehackControls[i] := TSpacehackGameControlKeypad.create;
+    //as a backup just create the superclass
     if not assigned(myspacehackControls[i]) then begin
       myspacehackControls[i] := TSpacehackGameControl.Create;
     end;
 
+    //if the control is a game control (not an instruction display) then tell it where to send value changes
     if myspacehackControls[i] is TSpacehackGameControl then begin
-      TSpacehackGameControl(myspacehackControls[i]).changeTopic:='clients/'+myIP+'/' + inttostr(i)+'/valuechanged';
+      TSpacehackGameControl(myspacehackControls[i]).changeTopic:='clients/'+myIP+'/' + inttostr(i)+'/value';
     end;
 
     myspacehackControls[i].hardware:=hardwareType;
@@ -677,7 +738,7 @@ var
   thisNode, thisDisplay, thisPins: TTreeNode;
   i, j: integer;
 begin
-
+  //loop through our controls and buses and put the data on the tree view.
   tvControls.Items.Clear;
 
   rootControlNode := tvControls.Items.Add(nil, 'Controls');
@@ -713,25 +774,25 @@ var
   myFile: TFileStream;
   myJSONRoot: TJSONObject;
 begin
+  //load the file to memory
   myFile := TFileStream.Create(configFile, fmOpenRead);
   if myFile = nil then begin
     showmessage('File load failed.');
   end else
   begin
     try
+      //parse the JSON in the file and store the JSON objects for later
       myJSONParser := TJSONParser.Create(myFile);
       myJSONRoot := TJSONObject(myJSONParser.Parse);
       localConfigJSON := TJSONObject(myJSONRoot.Find('local'));
       interfaceConfigJSON := TJSONObject(myJSONRoot.Find('interface'));
-
       controlsJSON := TJSONObject(localConfigJSON.Find('controls'));
       busesJSON := TJSONObject(localConfigJSON.Find('buses'));
 
+      //get my ip and the server ip
       myIP := interfaceConfigJSON.Find('ip').AsString;
       serverIP := localConfigJSON.Find('server').AsString;
-
       eServer.Text := serverIP;
-
       lblMyIP.Caption := 'My IP: ' + myIP;
       lblServerIP.Caption := 'Server IP:' + serverIP;
     except
@@ -741,8 +802,10 @@ begin
     myFile.Free;
   end;
 
+  //init some stuff with this data
   loadControlData;
   controlDataToTree;
+  InitialiseControls;
 end;
 
 end.
--- spacehackcontrolsinstructiondisplay.pas
+++ spacehackcontrolsinstructiondisplay.pas
@@ -11,14 +11,17 @@ uses
 
 type
   TSPacehackInstructionDisplay = class(TSpacehackControl)
-    instruction: string;
+    finstruction: string;
     timeAvailable: integer; //time available in milliseconds
     timeLeft: integer;
 
     progressBar: TProgressBar;
 
+    procedure newInstruction(value: string);
+
     procedure initUI(thisPanel:TPanel) override;
     procedure updateUI override;
+    property instruction: string read finstruction write newInstruction;
   end;
 
 implementation
@@ -35,6 +38,9 @@ begin;
     Max:=timeAvailable;
     Min := 0;
     Position:=timeLeft;
+    Smooth:=true;
+    //BarShowText:=true;
+    Step:=1;
     height := 20;
     top := thisPanel.Height - 24;
     Caption:='Time remaining';
@@ -43,9 +49,17 @@ begin;
   updateUI;
 end;
 
+
+procedure TSPacehackInstructionDisplay.newInstruction(value: string);
+begin
+  finstruction:=value;
+  timeLeft:=timeAvailable;
+end;
+
 procedure TSPacehackInstructionDisplay.updateUI;
 begin;
   aLabel.Caption:=instruction;
+  progressBar.Max:=timeAvailable;
   progressBar.Position:=timeLeft;
 end;
 
